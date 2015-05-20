b24e5df080a82af19fc4cea1e2ecf0363db07f00
sama <sama@luffy-vb.(none)>
2015-05-20 17:50:25 -0500

    Change variables names

--- phased_consensus_samtools.R
+++ phased_consensus_samtools.R
@@ -1,11 +1,11 @@
-# ./Phased_consensus.R REFERENCE FATHER MOTHER BED MASKED_CNS OUTPUT_NAME
+# ./Phased_consensus.R REFERENCE PHASE0 PHASE1 BED MASKED_CNS OUTPUT_NAME
 library(Biostrings)
 
 args <- commandArgs(trailingOnly = TRUE)
 
 ref<-readDNAStringSet(args[1], format="fasta")
-father<-readDNAStringSet(args[2], format="fasta")
-mother<-readDNAStringSet(args[3], format="fasta")
+phase0<-readDNAStringSet(args[2], format="fasta")
+phase1<-readDNAStringSet(args[3], format="fasta")
 bed<-read.table(args[4])
 mask<-readDNAStringSet(args[5], format="fasta")
 mask2<-mask
@@ -16,13 +16,13 @@ file.remove(paste(args[6],".choose_blocks.txt",sep=""))
 aln0 <- pairwiseAlignment(ref[[1]], mask[[1]], type="global") # Computes global alignment with Needleman-Wunsch algorithm. 
 minus<-start(insertion(aln0))
 startsub<-start(subject(aln0))-1
-for (i in 1:length(father)){
+for (i in 1:length(phase0)){
   start=bed[i,2]+1
   end=bed[i,3]
   startCMP<-start-startsub+sum(width(insertion(aln0))[start>=minus])
   endCMP<-end-startsub+sum(width(insertion(aln0))[end>=minus])
-  aln1 <- pairwiseAlignment(subseq(DNAString(as.character(pattern(aln0))),startCMP,endCMP), father[i], type="global") # Computes global alignment with Needleman-Wunsch algorithm. 
-  aln2 <- pairwiseAlignment(subseq(DNAString(as.character(pattern(aln0))),startCMP,endCMP), mother[i], type="global") # Computes global alignment with Needleman-Wunsch algorithm. 
+  aln1 <- pairwiseAlignment(subseq(DNAString(as.character(pattern(aln0))),startCMP,endCMP), phase0[i], type="global") # Computes global alignment with Needleman-Wunsch algorithm. 
+  aln2 <- pairwiseAlignment(subseq(DNAString(as.character(pattern(aln0))),startCMP,endCMP), phase1[i], type="global") # Computes global alignment with Needleman-Wunsch algorithm. 
   if (score(aln1)>score(aln2)) {
     write("0",file=paste(args[6],".choose_blocks.txt",sep=""),append=T)
   }else{
