5319193b5dd561866ba08df2e767c1c4180c22e7
sama <sama@luffy-vb.(none)>
2015-05-20 17:53:54 -0500

    Change variable names and add comments

--- cymt_numt_gatk.py
+++ cymt_numt_gatk.py
@@ -3,11 +3,13 @@ import re
 import argparse
 __author__='Sama'
 
-parser=argparse.ArgumentParser(description='This is a demo script to retrieve the cymt and numt by Sama.\nIt will give 3 output files; [output]_father.txt, [output]_mother.txt and [output].bed')
+parser=argparse.ArgumentParser(description='This is a script to convert the alleles in the blocks of the phased vcf into fasta format.\nThis fasta files are not separated in cymt numt yet.\nIt will give 3 output files; [output]_phase0.txt, [output]_phase1.txt and [output].bed')
 parser.add_argument('-i','--input',help='Input fasta file name',required=True)
-parser.add_argument('-v','--vcf',help='Input vcf file name',required=True)
+parser.add_argument('-v','--vcf',help='Input phased vcf file name',required=True)
 parser.add_argument('-o','--output',help='Output file name',required=True)
 args=parser.parse_args()
+
+# Save referene fasta in memory
 
 refseq=""
 with open(args.input,"r") as file1:
@@ -18,11 +20,11 @@ with open(args.input,"r") as file1:
 refseq=list(refseq)
 
 bed=open("%s.bed"%(args.output),"w")
-					
-seqfather=[]
-seqmother=[]
-seqfather.append("")
-seqmother.append("")
+
+seqphase0=[]
+seqphase1=[]
+seqphase0.append("")
+seqphase1.append("")
 i=0
 j=0
 nv=0
@@ -34,90 +36,99 @@ with open(args.vcf,"r") as file1:
 		if not re.match("#",line):
 			linesplit=line.split("\t")
 			linelast=linesplit[9].split(":")
+# Only analize biallelic snps
 			if len(linesplit[3]) == 1 and len(linesplit[4])==1:
+# Make the proper allele of every block depending on the phase tag in the vcf 
 				if int(linesplit[1])-1 != j:
-					seqmother[i]+="".join(list(refseq[j:int(linesplit[1])-1]))
-					seqfather[i]+="".join(list(refseq[j:int(linesplit[1])-1]))
+					seqphase1[i]+="".join(list(refseq[j:int(linesplit[1])-1]))
+					seqphase0[i]+="".join(list(refseq[j:int(linesplit[1])-1]))
 					j=int(linesplit[1])-1
 				j+=1
+# Starting a phased block or single snp
 				if"0/1" == linelast[0]:
 					if"LowQual" == linesplit[6]:
-						seqfather[i]+=linesplit[3]
-						seqmother[i]+=linesplit[3]
+						seqphase0[i]+=linesplit[3]
+						seqphase1[i]+=linesplit[3]
 					else:
 						if nv2 < 2:
-							seqfather[i]=""
-							seqmother[i]=""
+							seqphase0[i]=""
+							seqphase1[i]=""
 						else:
 							if int(linesplit[1])-1 != nv:
-								seqfather[i]=seqfather[i][:-(int(linesplit[1])-nv-1)]
-								seqmother[i]=seqmother[i][:-(int(linesplit[1])-nv-1)]
-							seqfather.append("")
-							seqmother.append("")
+								seqphase0[i]=seqphase0[i][:-(int(linesplit[1])-nv-1)]
+								seqphase1[i]=seqphase1[i][:-(int(linesplit[1])-nv-1)]
+							seqphase0.append("")
+							seqphase1.append("")
 							i+=1
 							bed.write("%s\t%s\t%s\n" %(linesplit[0],start-1,nv))
 						start=int(linesplit[1])
 						nv=int(linesplit[1])
-						seqfather[i]+=linesplit[3]
-						seqmother[i]+=linesplit[4]
+						seqphase0[i]+=linesplit[3]
+						seqphase1[i]+=linesplit[4]
 						nv2=1
+# Starting a phased block or homozygous snp
 				elif"1/1"==linelast[0]:
 					if"LowQual" == linesplit[6]:
-						seqfather[i]+=linesplit[3]
-						seqmother[i]+=linesplit[3]
+						seqphase0[i]+=linesplit[3]
+						seqphase1[i]+=linesplit[3]
 					else:
 						if nv2 < 2:
-							seqfather[i]=""
-							seqmother[i]=""
+							seqphase0[i]=""
+							seqphase1[i]=""
 						else:
 							if int(linesplit[1])-1 != nv:
-								seqfather[i]=seqfather[i][:-(int(linesplit[1])-nv-1)]
-								seqmother[i]=seqmother[i][:-(int(linesplit[1])-nv-1)]
-							seqfather.append("")
-							seqmother.append("")
+								seqphase0[i]=seqphase0[i][:-(int(linesplit[1])-nv-1)]
+								seqphase1[i]=seqphase1[i][:-(int(linesplit[1])-nv-1)]
+							seqphase0.append("")
+							seqphase1.append("")
 							i+=1
 							bed.write("%s\t%s\t%s\n" %(linesplit[0],start-1,nv))
 						start=int(linesplit[1])
 						nv=int(linesplit[1])
-						seqfather[i]+=linesplit[4]
-						seqmother[i]+=linesplit[4]
+						seqphase0[i]+=linesplit[4]
+						seqphase1[i]+=linesplit[4]
 						nv2=1
+# Phased heterozygous snp
 				elif"1|0" == linelast[0]:
-					seqfather[i]+=linesplit[4]
-					seqmother[i]+=linesplit[3]
+					seqphase0[i]+=linesplit[4]
+					seqphase1[i]+=linesplit[3]
 					nv=int(linesplit[1])
 					nv2+=1
+# Phased heterozygous snp
 				elif"0|1" == linelast[0]:
-					seqfather[i]+=linesplit[3]
-					seqmother[i]+=linesplit[4]
+					seqphase0[i]+=linesplit[3]
+					seqphase1[i]+=linesplit[4]
 					nv=int(linesplit[1])
 					nv2+=1
+# Phased homozygous snp
 				elif"1|1" == linelast[0]:
-					seqfather[i]+=linesplit[4]
-					seqmother[i]+=linesplit[4]
+					seqphase0[i]+=linesplit[4]
+					seqphase1[i]+=linesplit[4]
 					nv=int(linesplit[1])
 					nv2+=1
 				else:
 					print"weirdstuff"+linelast[0]+"\n"+line+"\n"
-	
+#For the last snp in the vcf file	
 if nv2 < 2:
-	seqfather.pop()
-	seqmother.pop()
+	seqphase0.pop()
+	seqphase1.pop()
 else:
 	bed.write("%s\t%s\t%s\n" %(linesplit[0],start-1,nv))
 
 bed.close()
 
+# Write the phased alleles in two files
+
 z=1
 with open(args.output + ".phase0.txt","w") as my_file:
-	for j in seqfather:
+	for j in seqphase0:
 		my_file.write(">" + args.output + "_phase0_" + str(z) + "\n")
 		z+=1
-		my_file.write(j + "\n")
+		my_file.write(j + "\n") 
 
 z=1
 with open(args.output + ".phase1.txt","w") as my_file:
-	for j in seqmother:
+	for j in seqphase1:
 		my_file.write(">" + args.output + "_phase1_" + str(z) + "\n")
 		z+=1
 		my_file.write(j + "\n")
